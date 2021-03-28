872c3d59fcafc257708739f745f6a938f0a9fd4c
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-03-28 15:43:12 +0300

    consumer bug fix

---
+++ consumers/KafkaConsumers.js
@@ -0,0 +1,132 @@
+const config = require('config');
+
+// Logger configuration
+const log4js = require('log4js');
+log4js.configure('./config/log4js-config.json');
+const loggerS = log4js.getLogger('statusConsumer');
+
+// Logger configuration
+const loggerR = log4js.getLogger('reportConsumer');
+
+
+const { Kafka } = require('kafkajs');
+const kafka = new Kafka({
+    clientId: config.get('kafka.consumers.status'),
+    brokers: [config.get('kafka.broker')]
+});
+
+
+/**
+ * Class consumer, which collect all request statuses
+ */
+class KafkaConsumers {
+    constructor() {
+        //report consumer run
+        this.reportHistory = [];
+        this.reportConsumer().catch(e => logger.error(`[example/consumer] ${e.message}`, e));
+ 
+        //status consumer run
+        this.statusHistory = [];
+        this.statusConsumer().catch(e => logger.error(`[example/consumer] ${e.message}`, e));
+    }
+
+    /**
+     * Method which consume status topic
+     */
+    async statusConsumer() {
+        try {
+
+            const consumer = kafka.consumer({ groupId: 'UIRequestStatuses' });
+            let topic = 'analysis-topic';
+
+            await consumer.connect();
+            loggerS.info('connected to: ' + topic);
+            await consumer.subscribe({ topic, fromBeginning: false });
+
+            //get each message and save it to statusHistory array
+            consumer.run({
+                eachMessage: async ({ topic, partition, message }) => {
+                    let msg = JSON.parse(message.value.toString());
+                    loggerS.info('Consumed statuse:' + JSON.stringify(msg));
+
+                    
+
+                    //Delete user requestes old statuses from history
+                    if (msg.status === 'COMPLETED') {
+                        console.log(msg.requestId + ' COMPLETED');
+                        this.statusHistory.forEach(element => {
+                            if (element.message.requestId === msg.requestId) {
+                                console.log(`deleted COMPLETED msg`);
+                                this.statusHistory.splice(this.statusHistory.indexOf(element), 1)
+                            } else console.log(`COMPLETED is false`);
+                        });
+                    } else {
+                        this.statusHistory.forEach(element => {
+                            if (element.message.requestId === msg.requestId &&
+                                message.timestamp >= element.timestamp) {
+                                console.log(`deleted NOT COMPLET msg`);
+                                this.statusHistory.splice(this.statusHistory.indexOf(element), 1)
+                            } else console.log(`NOT COMPLET`);
+                        });
+                    }
+                    this.statusHistory.push({
+                        message: msg,
+                        timestamp: message.timestamp
+                    });
+                }
+            });
+        } catch (e) {
+            loggerS.error('Error while consuming statuses. Message: ' + e);
+        }
+    }
+
+    async reportConsumer() {
+        try {
+
+            const consumer = kafka.consumer({ groupId: 'UIGetReport' });
+            let topic = 'reports';
+
+            await consumer.connect();
+            loggerR.info('connected to: ' + topic);
+            await consumer.subscribe({ topic, fromBeginning: false });
+
+            //get each message and save it to reportHistory array
+            consumer.run({
+                eachMessage: async ({ topic, partition, message }) => {
+                    let msg = JSON.parse(message.value.toString());
+                    loggerR.info('Consumed message: ' + msg);
+                    this.reportHistory.push({
+                        message: msg,
+                        timestamp: message.timestamp
+                    });
+
+                    //Delet old reports and save last 5 reports
+                    if (this.reportHistory.length > 5) {
+                        this.reportHistory.splice(0, this.reportHistory.length - 5);
+                    }
+
+                }
+            });
+        } catch (e) {
+            loggerR.error('Error while consuming reports. Message: ' + e);
+        }
+    }
+
+    /**
+     * 
+     * @returns reports from current history
+     */
+    getReportsHistory() {
+        return this.reportHistory;
+    }
+
+    /**
+     * 
+     * @returns status history from kafka
+     */
+    getStatusHistory() {
+        return this.statusHistory;
+    }
+}
+
+module.exports = new KafkaConsumers();
--- routes/status.js
+++ routes/status.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
-const StatusConsumer = require('../consumers/StatusConsumer');
+const KafkaConsumers = require('../consumers/KafkaConsumers');
 const router = Router();
 
-let statusConsumer = new StatusConsumer();
+let statusConsumer = KafkaConsumers;
 /**
  * Return latest status of all user's requestes
  */
@@ -17,5 +17,64 @@ router.post('/get', async (req, res) => {
     res.send(statusConsumer.getStatusHistory());
 });
 
+/**
+ * Statuses for testing
+ */
+ router.post('/getSimpleStatus', async (req, res) => {
+    //Simple array of user statuses
+    let resultStatuses = [
+        {
+            messages: {
+                requestId: '1111',
+                userId: '123212321323',
+                date: '12-12-12',
+                keywords: 'How to ...',
+                status: 'IN PROCESS'
+            },
+            timestamp: 10
+        },
+        {
+            messages: {
+                requestId: '2222',
+                userId: '123212321323',
+                date: '12-12-12',
+                keywords: 'Why...',
+                status: 'COMPLETED'
+            },
+            timestamp: 12
+        },
+        {
+            messages: {
+                requestId: '3333',
+                userId: '123212321323',
+                date: '12-12-12',
+                keywords: 'Any words...',
+                status: 'COMPLETED'
+            },
+            timestamp: 13
+        },
+        {
+            messages: {
+                requestId: '4444',
+                userId: '123212321323',
+                date: '12-12-12',
+                keywords: 'Wtf bro?',
+                status: 'IN PROCESS'
+            },
+            timestamp: 14
+        },
+        {
+            messages: {
+                requestId: '555',
+                userId: '123212321323',
+                date: '12-12-12',
+                keywords: 'Check bootstrap table border...',
+                status: 'COMPLETED'
+            },
+            timestamp: 15
+        }];
+    //Return latest statuses
+    res.send(resultStatuses);
+});
 
 module.exports = router;
