22e5ed2a0fae9640b855d19448910e8be6be6ae8
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-05-09 23:28:19 +0300

    Update status.js

--- routes/status.js
+++ routes/status.js
@@ -2,19 +2,26 @@ const { Router } = require('express');
 const KafkaConsumers = require('../consumers/KafkaConsumers');
 const router = Router();
 
-let statusConsumer = KafkaConsumers;
 /**
  * Return latest status of all user's requestes
  */
 router.post('/get', async (req, res) => {
 
     //If dosn't found any status, return null
-    if (statusConsumer.getStatusHistory() === null) {
+    if (KafkaConsumers.getStatusHistory() === null) {
         res.send(null);
     }
 
+    let userId = req.body.userId;
+    console.log(userId);
+
+    let statuses = KafkaConsumers.getStatusHistory().map(v=>{
+        if (v.message.userId===userId) {
+            return v;
+        }
+    })
     //Return latest statuses
-    res.send(statusConsumer.getStatusHistory());
+    res.send(statuses);
 });
 
 
